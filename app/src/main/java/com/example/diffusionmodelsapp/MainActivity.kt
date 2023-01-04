9ea0daa6c369333100f38afaa3a082b5ee758932
george <george@gigrev.com>
2023-01-04 16:44:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/diffusionmodelsapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.diffusionmodelsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.diffusionmodelsapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
