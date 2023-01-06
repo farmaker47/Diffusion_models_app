d2b2e3cf53afe9d7b251203fdea1c00688b4568b
george <george@gigrev.com>
2023-01-06 05:41:54 +0200

    Move code to fragment.

--- app/src/main/java/com/example/diffusionmodelsapp/MainActivity.kt
+++ app/src/main/java/com/example/diffusionmodelsapp/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.diffusionmodelsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.chaquo.python.Python
 import com.example.diffusionmodelsapp.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -16,12 +14,5 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
-
-        // Get string from Python
-        val python = Python.getInstance()
-        val pythonFile = python.getModule("wav_to_string")
-        //Log.v("Chaquopy", path + "heytj2.wav")
-        val wavString = pythonFile.callAttr("getStringFromWav", "georg")
-        Log.v("Chaquopy", wavString.toString())
     }
 }
--- app/src/main/java/com/example/diffusionmodelsapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/diffusionmodelsapp/ui/main/MainFragment.kt
@@ -2,10 +2,12 @@ package com.example.diffusionmodelsapp.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.chaquo.python.Python
 import com.example.diffusionmodelsapp.R
 
 class MainFragment : Fragment() {
@@ -26,7 +28,16 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val contentView = inflater.inflate(R.layout.fragment_main, container, false)
+
+        // Get string from Python
+        val python = Python.getInstance()
+        val pythonFile = python.getModule("encode_text")
+        //Log.v("Chaquopy", path + "heytj2.wav")
+        val wavString = pythonFile.callAttr("getStringFromWav", "georg")
+        Log.v("Chaquopy", wavString.toString())
+
+        return contentView
     }
 
 }
