d815db355dba93230f3d792ebc21494a6316bf92
george <george@gigrev.com>
2023-02-05 07:08:30 +0200

    Clean up python file.

--- app/src/main/python/run_diffusion_model.py
+++ app/src/main/python/run_diffusion_model.py
@@ -8,31 +8,13 @@ import requests
 # Run the diffusion model.
 # Change the ADDRESS parameter based on yours.
 def runDiffusionModel(context, unconditional_context):
-  #filename = join(dirname(__file__), "method.tflite")
-  #filename = "/data/data/com.example.diffusionmodelsapp/files/diffusion_model_17.tflite"
-  #print(filename)
-  # Load the TFLite model and allocate tensors.
-  #interpreter = tf.lite.Interpreter(model_path=filename)
-  #interpreter.allocate_tensors()
-
-  #context = np.array(context)
-  #unconditional_context = np.array(unconditional_context)
-  #print(type(context.tolist()))
-
   input_data_for_model = {
       'context' : np.array(context).tolist(),
       'unconditional_context' : np.array(unconditional_context).tolist()
   }
-  '''
-  print(type(np.array(context).tolist()))
-  input_data_for_model = {
-        'context' : 3,
-        'unconditional_context' : 4
-    }
-  '''
   data = json.dumps(input_data_for_model)
 
-  ADDRESS = "http://182b-35-237-48-189.ngrok.io/diffusion_model_inferenc"
+  ADDRESS = "http://6cc0-34-82-213-91.ngrok.io/diffusion_model_inference"
   response = requests.post(ADDRESS, data=data)
   json_response = json.loads(response.text)
   json_response = json.loads(json_response)
@@ -42,34 +24,3 @@ def runDiffusionModel(context, unconditional_context):
   print(np.array(json_response['output']).shape)
 
   return json_response['output'] #np.array(json_response['outputs'])
-
-# Run the decoder model.
-# Change the ADDRESS parameter based on yours.
-def runDecoderModel(latent):
-  latent = np.array(latent)
-
-  data = json.dumps(
-      {
-          "signature_name": "serving_default",
-          "inputs": {
-              "latent": latent.tolist(),
-          }
-  })
-
-  ADDRESS = "35.193.53.74"
-
-  headers = {
-      "content-type": "application/json"
-  }
-
-  response = requests.post(
-      f"http://{ADDRESS}:8501/v1/models/decoder:predict", data=data, headers=headers
-  )
-
-  json_response = json.loads(response.text)
-
-
-  print(type(json_response['outputs']))
-  print(np.array(json_response['outputs']).shape)
-
-  return np.array(json_response['outputs'])
